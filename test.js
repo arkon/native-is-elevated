0b8588334985ee88cf092e47d6142c3a9a00db7c
Eugene Cheung <ey.cheung@outlook.com>
2017-03-27 21:56:53 -0400

    Update tests

--- test.js
+++ test.js
@@ -1,17 +1,13 @@
 var test = require('ava').serial;
 var isElevated = require('.');
 
+// Note that 1 of the 2 tests will fail depending on if you're running
+// it normally, or with root/admin rights
+
 test('normal', t => {
-    t.false(isElevated());
+  t.false(isElevated());
 });
 
 test('elevated', t => {
-  if (process.platform === 'win32') {
-    // TODO
-  } else {
-    const _ = process.getuid;
-    process.getuid = () => 0;
-    t.true(isElevated());
-    process.getuid = _;
-  }
+  t.true(isElevated());
 });
