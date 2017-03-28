e6469753791527df7a06ad985279f85bd9f33c5d
Eugene Cheung <ey.cheung@outlook.com>
2017-03-28 11:47:54 -0400

    Assume we're using Unix if it's not Windows

--- src/is-elevated.cc
+++ src/is-elevated.cc
@@ -1,14 +1,13 @@
 #include <node.h>
 
-#ifdef linux
-#include <unistd.h>
+#if defined(_WIN32) || defined(WIN32)
+#include <windows.h>  // Windows
+#else
+#include <unistd.h>   // Unix (macOS, Linux)
 #endif
 
-#ifdef _WIN32
-#include <windows.h>
-#endif
-
-namespace is_elevated {
+namespace is_elevated
+{
   using v8::Boolean;
   using v8::FunctionCallbackInfo;
   using v8::Isolate;
@@ -16,36 +15,41 @@ namespace is_elevated {
   using v8::Object;
   using v8::Value;
 
-  void IsElevated(const FunctionCallbackInfo<Value>& args) {
+  void IsElevated(const FunctionCallbackInfo<Value>& args)
+  {
     Isolate *isolate = args.GetIsolate();
     bool bIsElevated = false;
 
-    #ifdef _WIN32
-      // Based on http://stackoverflow.com/a/8196291
-      HANDLE hToken = NULL;
-      if (OpenProcessToken(GetCurrentProcess(), TOKEN_QUERY, &hToken)) {
-        TOKEN_ELEVATION Elevation;
-        DWORD cbSize = sizeof(TOKEN_ELEVATION);
-        if (GetTokenInformation(hToken,
-                                TokenElevation,
-                                &Elevation,
-                                sizeof(Elevation),
-                                &cbSize)) {
-          bIsElevated = Elevation.TokenIsElevated;
-        }
+    #if defined(_WIN32) || defined(WIN32)
+    // Based on http://stackoverflow.com/a/8196291
+    HANDLE hToken = NULL;
+    if (OpenProcessToken(GetCurrentProcess(), TOKEN_QUERY, &hToken))
+    {
+      TOKEN_ELEVATION Elevation;
+      DWORD cbSize = sizeof(TOKEN_ELEVATION);
+      if (GetTokenInformation(hToken,
+                              TokenElevation,
+                              &Elevation,
+                              sizeof(Elevation),
+                              &cbSize))
+      {
+        bIsElevated = Elevation.TokenIsElevated;
       }
-      if (hToken) {
-        CloseHandle(hToken);
-      }
+    }
+    if (hToken)
+    {
+      CloseHandle(hToken);
+    }
     #else
-      bIsElevated = geteuid() == 0;
+    bIsElevated = geteuid() == 0;
     #endif
 
     Local<Boolean> resultBool = Boolean::New(isolate, bIsElevated);
     args.GetReturnValue().Set(resultBool);
   }
 
-  void Init(Local<Object> exports) {
+  void Init(Local<Object> exports)
+  {
     NODE_SET_METHOD(exports, "isElevated", IsElevated);
   }
 
